542224eaae09b37f445aa39fa2569f5929b56b6f
freddykibz <fred@kibzagency.com>
2020-02-13 23:52:59 +0300

    connected to mongoDb

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+//connect database
+connectDB();
+
 app.get('/',(req, res ) => res.send('API RUNNING'))
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server startes on port ${PORT}`));
